d2f952a57312db8c9590523dfe458cd70f4fc83b
boraaltinok <boraaltinok26@gmail.com>
2023-06-19 12:36:48 +0300

    flutter_gen added

---
+++ lib/configurations/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,15 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get tasks => 'مهام';
+
+  @override
+  String get teams => 'فرق';
+
+  @override
+  String get projects => 'المشاريع';
+}
---
+++ lib/configurations/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,15 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get tasks => 'Tasks';
+
+  @override
+  String get teams => 'Teams';
+
+  @override
+  String get projects => 'Projects';
+}
